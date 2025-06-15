d182ef1e7559afa1155f5a3a8d015bf66fce598a
Wkrotik <181585868+Wkrotik@users.noreply.github.com>
2025-06-15 15:21:03 +0400

    better tmux conf, also some changes to niri

---
+++ arch.sh
@@ -0,0 +1,16 @@
+#getting yay & stow
+sudo pacman -S --needed git base-devel stow -y
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+cd
+git clone https://github.com/Wkrotik/dotfiles
+cd dotfiles
+stow .
+#all needed pkgs
+yay -S --needed zsh brightnessctl fzf zoxite yazi kitty neovim appimagelauncher
+
+
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
